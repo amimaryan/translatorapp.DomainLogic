4166ff580c55b100ff004d55e67f051f84a989cc
Aret Mimaryan <amimaryan@gmail.com>
2019-08-10 18:13:10 -0400

    First Commit

---
+++ Models/LanguageModel.cs
@@ -0,0 +1,8 @@
+namespace translatorapp.DomainLayer
+{
+    public class LanguageModel
+    {
+        public string LanguageCode {get; set;}
+        public string Language {get; set;}
+    }
+}
---
+++ TranslatorLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace translatorapp.DomainLayer
+{
+    public class TranslatorLogic
+    {
+        public List<LanguageModel> GetLanguages()
+        {
+            var languageList = new List<LanguageModel>(){
+                new LanguageModel {LanguageCode = "ru", Language = "Russian"},
+                new LanguageModel {LanguageCode = "en", Language = "English"},
+                new LanguageModel {LanguageCode = "eu", Language = "Basque"}
+            };
+
+            return languageList;
+        }
+    }
+}
